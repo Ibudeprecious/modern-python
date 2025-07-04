323df4be05500d14dd8db04341b5d0dc556d3a84
Precious <ibudeprecious87@gmail.com>
2025-07-04 09:49:06 +0100

    added a table

--- streamlit_webapp.py
+++ streamlit_webapp.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+df = pd.DataFrame(data)
+
 
 st.title('Hi welcome to this page')
 st.subheader('Lets walk you around')
@@ -6,3 +15,4 @@ st.write('''Lets work you around a little I am ibude Precious.
          A python develoepr in training
          Welcome to this demo webapp\n
          I will publishing my projects here in the follwing weeks''')
+st.write(df)
