6b8c35a8ecdccd8365a94dbd446beaaf118b8c40
Precious <ibudeprecious87@gmail.com>
2025-07-04 10:14:17 +0100

    perfect

--- streamlit_webapp.py
+++ streamlit_webapp.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 
+Name = ['Precious', 'John', 'Doe']
 data = {
     'Income': [1000, 3000, 30000],
     'Age': [25, 30, 35],
-    'Name': ['Precious', 'John', 'Doe'],
 }
-df = pd.DataFrame(data, index=data['Name'])
+df = pd.DataFrame(data, index=Name)
+df.index.name = 'Name'
 
 
 st.title('Hi welcome to this page')
@@ -16,4 +17,4 @@ st.write('''Lets work you around a little I am ibude Precious.
          Welcome to this demo webapp\n
          I will publishing my projects here in the follwing weeks''')
 st.write(df)
-st.line_chart(df, x='Name', use_container_width=True)
+st.line_chart(df, use_container_width=True)
